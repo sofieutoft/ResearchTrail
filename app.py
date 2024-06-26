17828cbec94bd2ec82747cf29b14e3c8a7cb5b2a
sofieutoft <sofieu26@gmail.com>
2024-06-26 11:10:13 -0500

    Fixing style

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
 from flask import Flask, render_template, request
-from config import ARXIV_API_URL
+from config import ARXIV_API_URL, DATABASE_URI
 from data.fetch_data import get_request, extract_data
 from data.preprocess import preprocess_text
 from data.database import connect
 from data.recommend import get_recommendations
 import pandas as pd
+import sqlalchemy as db
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     engine = db.create_engine(DATABASE_URI)
     with engine.connect() as connection:
-        query_result = connection.execute(db.text("SELECT * FROM arXivPapers;")).fetchall()
+        query_result = connection.execute(
+            db.text("SELECT * FROM arXivPapers;")).fetchall()
         data = pd.DataFrame(query_result, columns=query_result[0].keys())
     return render_template('index.html', data=data)
+
 
 @app.route('/recommend', methods=['GET'])
 def recommend():
     paper_id = request.args.get('id')
     recommendations = get_recommendations(data, paper_id)
     return render_template('recommend.html', paper_id=paper_id, recommendations=recommendations)
+
 
 if __name__ == '__main__':
     # Fetch and process data
