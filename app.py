a961b532afbf7e531e052debe732481f2feb1469
Sofia Utoft <126016299+sofieutoft@users.noreply.github.com>
2024-07-06 21:14:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request
-from config import ARXIV_API_URL, DATABASE_URI
+from flask import Flask, render_template, url_for, flash, redirect, requestfrom config import ARXIV_API_URL, DATABASE_URI
 from data.fetch_data import get_request, extract_data
 from data.preprocess import preprocess_text
 from data.database import connect, fetch_all_papers
 from data.recommend import get_recommendations
 import pandas as pd
 import sqlalchemy as db
+import git
 
 
 app = Flask(__name__)
@@ -35,6 +35,16 @@ def recommend():
     # title = data['title'][data['link'] == paper_id]
     return render_template(
         'recommend.html', title=paper_id, recommendations=recommendations)
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/arXivProject/arXivproject)
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 
 if __name__ == '__main__':
